85eba82626fd734df85c95ec3d2f78b9e63601a0
Vincent Chang <vincentcch79@gmail.com>
2016-07-27 12:14:50 +0800

    change textfield subclass inset

--- BanannyDemo/ParentDashboardViewController.swift
+++ BanannyDemo/ParentDashboardViewController.swift
@@ -256,7 +256,7 @@ class ParentDashboardViewController: UIViewController, UIPickerViewDelegate, UIP
 
 class TextField: UITextField {
     
-    let padding = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 5);
+    let padding = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 5);
     
     override func textRectForBounds(bounds: CGRect) -> CGRect {
         return UIEdgeInsetsInsetRect(bounds, padding)
