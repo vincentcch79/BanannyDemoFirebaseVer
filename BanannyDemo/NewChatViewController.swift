57f208d8de8244cdae7c799916218e211213e2c2
Vincent Chang <vincentcch79@gmail.com>
2016-08-01 16:04:12 +0800

    change senderId

--- BanannyDemo/NewChatViewController.swift
+++ BanannyDemo/NewChatViewController.swift
@@ -29,10 +29,10 @@ class NewChatViewController: JSQMessagesViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "\(searchChatDetail.nameResult)保姆 訊息"
-//        self.senderId = "one"
-//        self.senderDisplayName = "Vinny"
-        self.senderId = "two"
-        self.senderDisplayName = "Jane"
+        self.senderId = "one"
+        self.senderDisplayName = "Vinny"
+//        self.senderId = "two"
+//        self.senderDisplayName = "Jane"
         self.view.backgroundColor = UIColor(red: 246/255, green: 246/255, blue: 246/255, alpha: 1)
         
         self.tabBarController?.tabBar.hidden = true
